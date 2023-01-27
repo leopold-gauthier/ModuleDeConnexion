91949d2d0688a00382b7914a7fb370a373c07ba1
leopold-gauthier <leopold.gauthier-de-porry@laplateforme.io>
2023-01-27 11:52:22 +0100

    commentaire supr

--- connect.php
+++ connect.php
@@ -1,10 +1,4 @@
 <?php
-// Informations d'identification
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_NAME', 'moduleconnexion');
-
 // Connexion à la base de données MySQL 
 $mysqli = new mysqli('localhost', 'root', '', 'moduleconnexion');
 //$mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
